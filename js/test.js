78c29d5e99e30b8e2bd9adc709fa0944e6f24eea
Andrey Morozov <andrey@morozov.lv>
2018-09-24 00:26:07 +0300

    moving to random point - done

--- js/test.js
+++ js/test.js
@@ -26,8 +26,23 @@ var getCentroid = function(path) {
     return new Point(x/3, y/3);
 }
 
+var getShift = function(path) {
+    return path.position - getCentroid(path);
+}
+
+var jumpTo = function(path, point) {
+
+    path.position += point - getCentroid(path);
+}
+
+var jumpTowards = function(path, vector) {
+
+    path.position += vector;
+}
+
 //////////////////////////////////////////////////////
 // Create a centered text item at the center of the view:
+
 var boid = new Path();
 boid.strokeColor = 'white';
 boid.add(new Point(0, 60));
@@ -35,31 +50,43 @@ boid.add(new Point(8, 30));
 boid.add(new Point(16, 60));
 boid.scaling *= 2;
 boid.closed = true;
+boid.fullySelected = true;
 
-var shift = 0; //getCentroid(boid) - boid.position;
-boid.position += (view.center - getCentroid(boid)) + shift;
+boid.rotate(90, getCentroid(boid));
 
-// aligning boid along X axys (0 degrees)
-boid.rotate(90);
+var center = drawCircle(view.center, 3, 'orange');
 
 var destination = getDecision("").dest;
-var vector = destination - boid.position;
+var dest = drawCircle(destination, 2, 'white');
+var vector = destination - getCentroid(boid);
+boid.rotate(vector.angle);
+vector = destination - getCentroid(boid);
+vector.length = 5;
+//jumpTo(boid, destination);
 
-drawCircle(view.center, 4, 'orange');
-drawCircle(destination, 2, 'white');
+
+//boid.rotate(vector.angle);
+//var prevAngle = -vector.angle;
+/*
 drawCircle(boid.position, 2, 'green');
 drawCircle(getCentroid(boid), 2, 'red');
 console.log(getCentroid(boid));
 var boidPosition = drawCircle(boid.position, 2, 'yellow');
+*/
 
-var prevAngle = -vector.angle;
-boid.fullySelected = true;
+var centroid = drawCircle(getCentroid(boid), 2, 'red');
 
 document.querySelector('#btn-1').addEventListener('click', function(e) {
+
 	boid.rotate(45, getCentroid(boid));
-	boidPosition.position = boid.position;
+	//boidPosition.position = boid.position;
 });
 
+
 document.querySelector('#btn-2').addEventListener('click', function(e) {
-	boid.position -= new Point(10, 0);
+
+    //boid.position -= new Point(10, 0);
+    //jumpTo(boid, destination);
+
+    jumpTowards(boid, vector);
 });
