d1eeda709fbad8fe5ea672fa84e9f6e88348d012
Andrey Morozov <andrey@morozov.lv>
2018-10-09 22:18:07 +0300

    getAttractionForces - done

--- js/test.js
+++ js/test.js
@@ -5,26 +5,58 @@ var drawVectorFromPoint = function(point, vector, color) {
     new Path.Circle(vector, 2).strokeColor = color;
 }
 
-var zero = new Point([0, 0]);
+var getRandomPoint = function() {
+    return Point.random() * view.size;
+}
 
-var v1 = new Point(20, 30);
-console.log(v1.x + ' / ' + v1.y + ' / '+v1.angle + ' / '+v1.length);
-drawVectorFromPoint(zero, v1, 'white');
+var getRandomAngle = function() {
+    return -180 + Math.random() * 180;
+}
 
-var v2 = new Point(30, 20);
-console.log(v2.x + ' / ' + v2.y + ' / '+v2.angle + ' / '+v2.length);
-drawVectorFromPoint(zero, v2, 'lime');
+var getAttractionForces = function(init_force, boid, flock, distanceToScan) {
 
-var v3 = v2 - v1;
-console.log(v3.x + ' / ' + v3.y + ' / '+v3.angle + ' / '+v3.length);
-drawVectorFromPoint(v1, v2, 'yellow');
+    var centroid = boid.get_centroid();
+    var name = boid.name;
 
-////////////////////////////////////////////////////////////
+    return flock
+    .filter(function (flock_boid) {
+        return name !== flock_boid.name;
+    })
+    .map(function (flock_boid) {
+        return flock_boid.get_centroid();
+    })
+    .filter(function(flock_centroid) {
+        var distanceVector = centroid - flock_centroid;
 
+        return distanceVector.length > 0 && distanceVector.length <= distanceToScan;
+    })
+    .reduce(function (accumulativeVector, flock_centroid) {
+        
+        var distanceVector = centroid - flock_centroid;
+
+        drawVectorFromPoint(flock_centroid, centroid, 'yellow');      
+
+        return accumulativeVector + distanceVector;
+    }, init_force);
+}
+
+
+var b1 = new Boid('white', 'boid1', getRandomPoint(), getRandomAngle(), 1, 1, dm2().map);
+var b2 = new Boid('red', 'boid2', getRandomPoint(), getRandomAngle(), 1, 1, dm2().map);
+var b3 = new Boid('lime', 'boid3', getRandomPoint(), getRandomAngle(), 1, 1, dm2().map);
+
+var radius = 200;
+var f = [b2, b3];
+var start_force = new Point(0, 0);
+var v1 = getAttractionForces(start_force, b1, f, radius);
+
+new Path.Circle(b1.get_centroid(), radius).strokeColor = 'yellow';
+
+drawVectorFromPoint(b1.get_centroid(), b1.get_centroid() + v1, 'white');
+
+/*
 document.querySelector('#btn-1').addEventListener('click', function(e) {
-
-	//boid.rotate(45, getCentroid(boid));
-	//boidPosition.position = boid.position;
+    
 });
 
 
@@ -35,3 +67,4 @@ document.querySelector('#btn-2').addEventListener('click', function(e) {
 
     //jumpTowards(boid, vector);
 });
+*/
